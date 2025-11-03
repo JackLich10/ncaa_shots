d31916b298299622f026da464dada36135944679
jack lichtenstein <jack.lichtenstein@duke.edu>
2025-11-03 09:44:43 -0500

    try bigger chromote timeout

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,7 @@
 # Utility functions
 
+options(chromote.timeout = 60) # Set chromote timeout to 60 seconds
+
 get_current_season <- function() {
   season <- ifelse(as.integer(substr(Sys.Date(), 6, 7)) >= 11,
                    as.integer(substr(Sys.Date(), 1, 4)) + 1,
